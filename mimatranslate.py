8efa9f9449c976144522e1abed01fb4a41c47802
Misael Marcelino <misael.marcelino@outlook.com>
2022-12-08 19:20:41 -0300

    Finalizada construção do Tradutor

--- mimatranslate.py
+++ mimatranslate.py
@@ -5,46 +5,74 @@
 # Botão para tradução
 
 
-from tkinter import Tk, ttk, Text
+from tkinter import YES, Tk, ttk, Text, Button
 from googletrans import Translator
 
 translator = Translator()
 
-def traduzir():
-    texto=''
-    src='',
-    dest=''
-    
-    resultado= translator.translate()
-    print(resultado.text)
-    return resultado
+
+def traduzir(evento=None):
+    texto = entradaTexto.get('1.0', 'end')
+    src = comboEntrada.get()
+    dest = comboSaida.get()
+    resultado = translator.translate(text=texto, src=src, dest=dest)
+
+    traducaoTexto.configure(state='normal')
+    traducaoTexto.delete('1.0', 'end')
+    traducaoTexto.insert('1.0', resultado.text)
+    traducaoTexto.configure(state='disabled')
+
 
 # Criando janela com TKinter
 janela = Tk()
 janela.title('Mima Tradutor')
 
-values = ['pt','es','en']
+values = ['pt', 'es', 'en']
 
-box = ttk.Frame(width=450, height=100,)
-#Entrada de tradução
+box = ttk.Frame(width=450,)
+
+# Entrada de tradução
 frameEntrada = ttk.Frame()
-labelEntrada = ttk.Label(frameEntrada, text='Escolha o idioma de tradução: ', font=('Roboto', 15) )
-comboEntrada = ttk.Combobox(frameEntrada, values=values, font=('Roboto', 15) )
+labelEntrada = ttk.Label(
+    frameEntrada, text='Escolha o idioma: ', font=('Roboto', 15))
+comboEntrada = ttk.Combobox(frameEntrada,
+                            values=values,
+                            font=('Roboto', 15))
 comboEntrada.set('pt')
-entradaTexto = Text(width=50, height=10)
 
 labelEntrada.grid(row=0, column=0, )
 comboEntrada.grid(row=0, column=1)
 frameEntrada.pack(padx=15, pady=10)
 
-entradaTexto.pack(padx=10, fill='both', expand='yes')
+entradaTexto = Text(width=50, height=10, font=('Roboto', 15), pady=10, padx=5)
+entradaTexto.pack(padx=10, fill='both', expand="yes")
 
-#Saída da tradução
+# Saída da tradução
+frameSaida = ttk.Frame()
+labelSaida = ttk.Label(
+    frameSaida, text='Traduzindo para: ', font=('Roboto', 15))
+comboSaida = ttk.Combobox(frameSaida,
+                          values=values,
+                          font=('Roboto', 15))
+comboSaida.set('en')
+
+labelSaida.grid(row=0, column=0, )
+comboSaida.grid(row=0, column=1)
+frameSaida.pack(padx=15, pady=10)
+
+traducaoTexto = Text(width=50,
+                     height=10,
+                     font=('Roboto', 15),
+                     pady=10,
+                     padx=5,
+                     state='disabled'
+                     )
+traducaoTexto.pack(padx=10, fill='both', expand="YES")
+
+btnTraduzir = Button(text='Traduzir!', font=('Roboto', 15), command=traduzir)
+btnTraduzir.pack(fill='both', padx=10, pady=10)
+
+janela.bind('<Return>', traduzir)
 
 box.pack()
-
-
 janela.mainloop()
-
-
-
